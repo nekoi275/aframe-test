6c1015bca34b042f350dbdb12474ec9b8336b688
nekoi <nekoi275@gmail.com>
2023-08-31 14:57:28 +0300

    event change to click

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 AFRAME.registerComponent('change-on-hover', {
     schema: {
-        sound: {type: 'string'},
-        sky: {type: 'string'},
-        floor: {type: 'string'},
-        next: {type: 'string'}
+        sound: { type: 'string' },
+        sky: { type: 'string' },
+        floor: { type: 'string' },
+        next: { type: 'string' }
     },
 
     init: function () {
@@ -14,8 +14,6 @@ AFRAME.registerComponent('change-on-hover', {
             document.querySelector(`#${data.sound}`).components.sound.playSound();
             document.querySelector('a-sky').setAttribute('src', `#${data.sky}`);
             document.querySelector('a-plane').setAttribute('src', `#${data.floor}`);
-        });
-        el.addEventListener('mouseleave', function () {
             el.setAttribute('scale', '0 0 0')
             document.querySelector(`#${data.next}`).setAttribute('scale', '1 1 1');
         });
